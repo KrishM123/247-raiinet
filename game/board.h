f49a2ff7876f2a3442d38d55f0790168e412c70e
Akshar Barot <akshar.barot67@gmail.com>
2025-07-25 18:15:13 -0400

    board.h

--- game/board.h
+++ game/board.h
@@ -1,4 +1,21 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "cell.h"
+#include "occupant.h"
+#include "position.h"
+#include <vector>
+#include <memory>
+
+class Board
+{
+  std::vector<std::vector<std::unique_ptr<Cell>>> grid;
+
+public:
+  bool isValidPosition();
+  Cell &getCell(Position pos);
+  void placeOccupant(std::shared_ptr<Occupant> occupant, const Position &pos);
+  void removeOccupant(std::shared_ptr<Occupant> occupant, const Position &pos);
+};
+
 #endif // BOARD_H
