6e1efa28132fe1807b3cf45d74549f6163dbdd41
Akshar Barot <akshar.barot67@gmail.com>
2025-07-29 22:39:54 -0400

    finished graphics

--- graphics/x_window.h
+++ graphics/x_window.h
@@ -9,7 +9,7 @@ class Xwindow {
   Window w;
   int s, width, height;
   GC gc;
-  unsigned long colours[9];
+  unsigned long colours[12];
 
 public:
   Xwindow(int width = 500,
@@ -25,7 +25,10 @@ public:
     Gray50,
     Black,
     Darkseagreen1,
-    Lightcoral
+    Lightcoral,
+    Darkorange,
+    Gray75,
+    Slategray1
   }; // Available colours.
 
   int getWidth() const;
